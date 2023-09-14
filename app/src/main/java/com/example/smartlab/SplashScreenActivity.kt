573eb9cbe84d14eaee6337efeff7dadd887552f3
Анастасия Ческина <ncheskina13@gmail.com>
2023-09-14 21:58:59 +0300

    Реализован загрузочный экран, добавлен переход на страницу с ознакомительной информацией о приложении

--- app/src/main/java/com/example/smartlab/SplashScreenActivity.kt
+++ app/src/main/java/com/example/smartlab/SplashScreenActivity.kt
@@ -1,11 +1,20 @@
 package com.example.smartlab
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashScreenActivity : AppCompatActivity() {
+    private val SPLASH_TIME_OUT = 3000L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_screen_activity)
+        Handler().postDelayed( // задержка текущего экрана с дальнейшим переходом на другую активность
+            {
+                val i = Intent(this@SplashScreenActivity, OnBoardFirstActivity::class.java)
+                startActivity(i)
+                finish()
+            }, SPLASH_TIME_OUT)
     }
 }
