dbbb19c3a232ec29c0c52c18b842c3d63928c0e6
Анастасия Ческина <ncheskina13@gmail.com>
2023-09-14 22:16:11 +0300

    Добавлено две активности (для свайпов в будущем)

---
+++ app/src/main/java/com/example/smartlab/OnBoardSecondActivity.kt
@@ -0,0 +1,11 @@
+package com.example.smartlab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnBoardSecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.on_board_second_activity)
+    }
+}
---
+++ app/src/main/java/com/example/smartlab/OnBoardThirdActivity.kt
@@ -0,0 +1,11 @@
+package com.example.smartlab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OnBoardThirdActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.on_board_third_activity)
+    }
+}
