66da89a6b5cb0315ace5bc50adab1e5d6986b596
Анастасия Ческина <ncheskina13@gmail.com>
2023-09-19 21:19:49 +0300

    Реализован функционал свайпа начальных страниц

--- app/src/main/java/com/example/smartlab/OnBoardFirstActivity.kt
+++ app/src/main/java/com/example/smartlab/OnBoardFirstActivity.kt
@@ -1,11 +1,71 @@
 package com.example.smartlab
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.widget.Toast
 
-class OnBoardFirstActivity : AppCompatActivity() {
+class OnBoardFirstActivity : AppCompatActivity(), GestureDetector.OnGestureListener {
+
+    lateinit var gestureDetector: GestureDetector //Объект
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.on_board_first_activity)
+        gestureDetector = GestureDetector(this, this)
     }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        return if(gestureDetector.onTouchEvent(event!!)){
+            true
+        }else{
+            return super.onTouchEvent(event)
+        }
+    }
+
+    override fun onDown(p0: MotionEvent): Boolean {
+        return false
+    }
+
+    override fun onShowPress(p0: MotionEvent) {
+
+    }
+
+    override fun onSingleTapUp(p0: MotionEvent): Boolean {
+        return false
+    }
+
+    override fun onScroll(p0: MotionEvent, p1: MotionEvent, p2: Float, p3: Float): Boolean {
+        return false
+    }
+
+    override fun onLongPress(p0: MotionEvent) {
+
+    }
+
+    override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
+        val diffY = e2.y - e1.y
+        val diffX = e2.x - e1.x
+        if(Math.abs(diffX) > Math.abs(diffY)){
+            if(Math.abs(diffX) >100 && Math.abs(velocityX) > 100){
+                //влево
+                if(diffX<0){
+                    val intent = Intent(this,OnBoardSecondActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+        }
+        return true
+    }
+
+    //if(diffX<0){
+    //    Toast.makeText(this,"Смахиваем влево", Toast.LENGTH_LONG).show()
+    //}
+    //else {
+//   Toast.makeText(this,"Смахиваем вправо", Toast.LENGTH_LONG).show()
+    //}
+
+
 }
--- app/src/main/java/com/example/smartlab/OnBoardSecondActivity.kt
+++ app/src/main/java/com/example/smartlab/OnBoardSecondActivity.kt
@@ -1,11 +1,64 @@
 package com.example.smartlab
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
 
-class OnBoardSecondActivity : AppCompatActivity() {
+class OnBoardSecondActivity : AppCompatActivity(), GestureDetector.OnGestureListener {
+
+    lateinit var gestureDetector: GestureDetector //Объект
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.on_board_second_activity)
+        gestureDetector = GestureDetector(this, this)
+    }
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        return if(gestureDetector.onTouchEvent(event!!)){
+            true
+        }else{
+            return super.onTouchEvent(event)
+        }
+    }
+
+    override fun onDown(p0: MotionEvent): Boolean {
+        return false
+    }
+
+    override fun onShowPress(p0: MotionEvent) {
+
+    }
+
+    override fun onSingleTapUp(p0: MotionEvent): Boolean {
+        return false
+    }
+
+    override fun onScroll(p0: MotionEvent, p1: MotionEvent, p2: Float, p3: Float): Boolean {
+        return false
+    }
+
+    override fun onLongPress(p0: MotionEvent) {
+
+    }
+
+    override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
+        val diffY = e2.y - e1.y
+        val diffX = e2.x - e1.x
+        if(Math.abs(diffX) > Math.abs(diffY)){
+            if(Math.abs(diffX) >100 && Math.abs(velocityX) > 100){
+                //влево
+                if(diffX<0){
+                    val intent = Intent(this,OnBoardThirdActivity::class.java)
+                    startActivity(intent)
+                }
+                //вправо
+                else {
+                    val intent = Intent(this,OnBoardFirstActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+        }
+        return true
     }
 }
